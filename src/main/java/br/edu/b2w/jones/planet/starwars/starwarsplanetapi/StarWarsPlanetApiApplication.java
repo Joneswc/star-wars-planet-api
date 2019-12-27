16217a683bd2f743d427ac1564678ae8df12e049
Joneswc <joneswc@gmail.com>
2019-12-27 09:54:07 -0300

    initial commit

---
+++ src/main/java/br/edu/b2w/jones/planet/starwars/starwarsplanetapi/StarWarsPlanetApiApplication.java
@@ -0,0 +1,13 @@
+package br.edu.b2w.jones.planet.starwars.starwarsplanetapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarWarsPlanetApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarWarsPlanetApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/b2w/jones/planet/starwars/starwarsplanetapi/StarWarsPlanetApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.b2w.jones.planet.starwars.starwarsplanetapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarWarsPlanetApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
